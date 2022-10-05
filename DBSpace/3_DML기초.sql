d8ab06df79c8c4a30c1039fea03ce4a99cd67838
HyunJng <khjung1654@gmail.com>
2022-10-05 13:56:00 +0900

    DB과제 ERD

--- DBSpace/3_DML기초.sql
+++ DBSpace/3_DML기초.sql
@@ -22,6 +22,10 @@ values ('스포츠 의학4', 17, '한솔의학서적', 90000);
 
 select * from book;
 
+insert into dept01
+select * from dept where rownum <= 3;
+
+select * from dept01;
 rollback;
 -- 9. update문 : 수정
 -- 주의 :  UPDATE SQL문장을 사용할 때 반드시 where 조건문을 사용해야 함. (사용하지 않으면 모든 형의 속성값을 변경)
@@ -41,9 +45,64 @@ set address = (select address
             where name = '김연아')
 where name like '박세리';
 
+update emp01
+set sal = sal * 1.1
+where deptno = 30;
+
+update emp01
+set deptno = 20, job = 'MANAGER'
+where ename = 'SCOTT';
+
+select * from emp01;
+select * from dept01;
+
+update dept01
+set (dname, loc) = (select dname, loc from dept01 where deptno = 10)
+where deptno = 30;
+rollback;
 -- 10. delete문
 DELETE FROM Customer
 WHERE custid=5;
 select * from customer;
 -- 참조 무결성 제약조건이 restricted이라서 DELETE불가능
 DELETE FROM Customer;
+
+delete from dept01
+where deptno = 30;
+
+-- INSERT ALL
+truncate table emp02;
+select * from emp02;
+select * from emp01;
+select * from emp;
+
+desc emp01;
+desc emp02;
+
+insert all
+into emp01 values(empno, ename, job, deptno)
+into emp02 values(empno, ename)
+select * 
+from emp
+where deptno = 20;
+
+rollback;
+
+insert all
+when deptno = 30 then
+into emp01 values(empno, ename, job, deptno)
+when ename = 'SCOTT' then
+into emp02 values(empno, ename)
+select * from emp;
+
+select * from emp01;
+select * from emp02;
+
+insert all
+when deptno = 30 then
+into emp01 values(empno, ename, job, deptno)
+when hiredate > '1982/01/01' then
+into emp02 values(empno, ename)
+select * from emp;
+
+select * from emp02;
