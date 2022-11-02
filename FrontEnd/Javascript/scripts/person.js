c0eebdd8ddd08603b64165b94e5a348de4904009
KHJ <khjung1654@gmail.com>
2022-11-02 21:46:36 +0900

    22.11.02 수업 코드: JS의 function, object, module, json

---
+++ FrontEnd/Javascript/scripts/person.js
@@ -0,0 +1,6 @@
+// javascript library file을 별도로 만들 수 있다.
+// 전체 web application에서 사용하는 변수, function 등을 library화 한 것.
+// 다른 javascript source code에서 import 명령어를 사용하여 library 사용 가능
+// 다른 javascript source code에서 사용할 변수, function들은 앞에 export 키워드를 사용.
+export const name = "Jesse";
+export const age = 40;
