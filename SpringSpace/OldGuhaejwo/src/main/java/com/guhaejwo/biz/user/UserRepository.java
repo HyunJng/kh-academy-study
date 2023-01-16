56aa3ef3cdb4fe4937720acb37cac77568cc8b56
HyunJng <khjung1654@gmail.com>
2023-01-17 00:27:40 +0900

    자바 버전 1.8로 수정 완료

---
+++ SpringSpace/OldGuhaejwo/src/main/java/com/guhaejwo/biz/user/UserRepository.java
@@ -0,0 +1,19 @@
+package com.guhaejwo.biz.user;
+
+import java.util.List;
+
+public interface UserRepository {
+	
+	// 회원가입
+	public void save(UserDTO user);
+	
+	// 회원 탈퇴
+	public void delete(UserDTO user);
+	
+	// 회원 검색
+	public UserDTO getUser(UserDTO user);
+	
+	// 회원 리스트 가져오기
+	public List<UserDTO> getUserList();
+	
+}
